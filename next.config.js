7522c15d1197eead083807a0b74b2485db15b13f
Brayan Hernandez <brayannhc07@gmail.com>
2025-09-27 19:17:52 -0600

    testing app service

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    distDir: 'build',
+    output: 'standalone',
+    env: {
+      CUSTOM_API_URL: process.env.CUSTOM_API_URL,
+    },
+  };
+  
+  module.exports = nextConfig;
