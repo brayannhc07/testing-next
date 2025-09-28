e127f153b1b3942d6e605d1a1b35913124645da5
Brayan Hernandez <brayannhc07@gmail.com>
2025-09-27 19:22:43 -0600

    update env variables

--- next.config.js
+++ next.config.js
@@ -4,7 +4,13 @@ const nextConfig = {
     distDir: 'build',
     output: 'standalone',
     env: {
-      CUSTOM_API_URL: process.env.CUSTOM_API_URL,
+      PROPIO_BACKEND_URL: process.env.PROPIO_BACKEND_URL,
+      AUTH0_SECRET: process.env.AUTH0_SECRET,
+      APP_BASE_URL: process.env.APP_BASE_URL,
+      AUTH0_DOMAIN: process.env.AUTH0_DOMAIN,   
+      AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID,
+      AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
+      AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
     },
   };
   
